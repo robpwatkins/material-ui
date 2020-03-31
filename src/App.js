22829b78ca5f74591449f39e11bb510034c5cdb7
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-03-31 09:31:18 -0500

    got purple button majesty

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
 import React from 'react';
 import './App.css';
-import { AppBar, Fab, Box, InputBase } from '@material-ui/core/';
-import { Menu, Search } from '@material-ui/icons';
+import { Button, Typography } from '@material-ui/core/';
+import { makeStyles } from '@material-ui/core/styles';
 
-class App extends React.Component {
+function App () {
+  const useStyles = makeStyles ({
+    heyooStyle: {
+      fontStyle: 'oblique'
+    }
+  })
 
-  render() {
+    const classes = useStyles();
     return (
-      <div style={{width: '100%'}} className="App">
-          <AppBar color="primary">
-            <Box 
-              display='flex' 
-              alignItems='center'
-              width='75%'
-              height='9vh'
-              justifyContent='space-around'
-              paddingLeft='15px'
-              >
-              <Menu />
-              <h2 style={{margin: '0'}}>Title</h2>
-              <div style={{display: 'flex'}}>
-                <div>
-                  <Search />
-                </div>
-                <InputBase 
-                  placeholder='search . . .'
-                />
-              </div>
-            </Box>
-          </AppBar>
-        {/* <Fab variant="round" color="primary">hey.</Fab> */}
+      <div className="App">
+        <Typography className={classes.heyooStyle} variant='h5' color='secondary'>
+          Heyoo
+        </Typography>
+        <Button color='primary' variant='contained'>Heyoo</Button>
       </div>
     );
-  }
 }
 
 export default App;
