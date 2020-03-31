751c16873ab8a849cdd2558c1b126743a7f4dc9f
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-03-30 19:10:22 -0500

    a lil tedious

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import './App.css';
 // import PupsList from './components/PupsList';
-import { Fab } from '@material-ui/core/';
+import { AppBar, Fab, Box, InputBase } from '@material-ui/core/';
+import { Menu, Search } from '@material-ui/icons';
 
 class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <Fab variant="round" color="primary">hey.</Fab>
+      <div style={{width: '100%'}} className="App">
+          <AppBar color="primary">
+            <Box 
+              display='flex' 
+              alignItems='center'
+              width='75%'
+              height='9vh'
+              justifyContent='space-around'
+              paddingLeft='15px'
+              >
+              <Menu />
+              <h2 style={{margin: '0'}}>Title</h2>
+              <div style={{display: 'flex'}}>
+                <div>
+                  <Search />
+                </div>
+                <InputBase 
+                  placeholder='search . . .'
+                />
+              </div>
+            </Box>
+          </AppBar>
+        {/* <Fab variant="round" color="primary">hey.</Fab> */}
       </div>
     );
   }
