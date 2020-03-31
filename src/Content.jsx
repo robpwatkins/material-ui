aa031f9bf56af9b736a4d7040d06f0a8a4a04b3c
Rob W <robpwatkins@gmail.com>
2020-03-31 14:31:20 -0500

    look at them lil pups

--- src/Content.jsx
+++ src/Content.jsx
@@ -5,7 +5,52 @@ import { Grid } from '@material-ui/core';
 const Content = () => {
   return (
     <Grid container spacing={4}>
-      <Grid item xs={12} sm={4}>
+      <Grid item xs={12} sm={6} md={4}>
+        <CoffeeCard 
+          title="Boopy Pup" 
+          subtitle="$2.99" 
+          avatarSrc='https://i.redd.it/keszh72c3io01.jpg' 
+          imgSrc='https://i.redd.it/keszh72c3io01.jpg'
+          description='A tiny lil boop.'
+           />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <CoffeeCard 
+          title="Boopy Pup" 
+          subtitle="$2.99" 
+          avatarSrc='https://i.redd.it/keszh72c3io01.jpg' 
+          imgSrc='https://i.redd.it/keszh72c3io01.jpg'
+          description='A tiny lil boop.'
+           />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <CoffeeCard 
+          title="Boopy Pup" 
+          subtitle="$2.99" 
+          avatarSrc='https://i.redd.it/keszh72c3io01.jpg' 
+          imgSrc='https://i.redd.it/keszh72c3io01.jpg'
+          description='A tiny lil boop.'
+           />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <CoffeeCard 
+          title="Boopy Pup" 
+          subtitle="$2.99" 
+          avatarSrc='https://i.redd.it/keszh72c3io01.jpg' 
+          imgSrc='https://i.redd.it/keszh72c3io01.jpg'
+          description='A tiny lil boop.'
+           />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <CoffeeCard 
+          title="Boopy Pup" 
+          subtitle="$2.99" 
+          avatarSrc='https://i.redd.it/keszh72c3io01.jpg' 
+          imgSrc='https://i.redd.it/keszh72c3io01.jpg'
+          description='A tiny lil boop.'
+           />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
         <CoffeeCard 
           title="Boopy Pup" 
           subtitle="$2.99" 
