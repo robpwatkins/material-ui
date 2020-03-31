390b0264c72ce080f950a09480d80da19980cb01
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-03-31 11:33:22 -0500

    look at that card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import './App.css';
-import { Button, Typography } from '@material-ui/core/';
-import { makeStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
+import Header from './Header';
+import Content from './Content';
 
 function App () {
-  const useStyles = makeStyles ({
-    heyooStyle: {
-      fontStyle: 'oblique'
-    }
-  })
-
-    const classes = useStyles();
     return (
-      <div className="App">
-        <Typography className={classes.heyooStyle} variant='h5' color='secondary'>
-          Heyoo
-        </Typography>
-        <Button color='primary' variant='contained'>Heyoo</Button>
-      </div>
+      <Grid container direction="column">
+        <Grid item>
+          <Header />
+        </Grid>
+        <Grid item container>
+          <Grid item xs={false} sm={2} />
+          <Grid item xs={12} sm={8}>
+          <Content />
+          </Grid>
+          <Grid item xs={false} sm={2} />
+        </Grid>
+      </Grid>
     );
 }
 
